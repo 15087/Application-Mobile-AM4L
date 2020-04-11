4fed70db3e3158adfb0966ebd4b0e1bc7c6ec7cd
16131 <zakitylox@gmail.com>
2020-04-11 22:14:33 +0200

    Added notice Services

---
+++ app_mobile/lib/services/noticeServices.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NoticeService {
+  final String uid;
+  NoticeService({this.uid});
+
+  // collection reference
+  final CollectionReference noticeCollection =
+      Firestore.instance.collection('notices');
+
+  Future updateNoticeData(String title, String body) async {
+    return await noticeCollection.document(uid).setData({
+      'title': title,
+      'body': body,
+    });
+  }
+}
