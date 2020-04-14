3e940793b6466c2f7449a9e16ad2a9d8b1ffa211
Hana-Jabbour <36258812+Hana-Jabbour@users.noreply.github.com>
2020-04-14 15:39:45 +0200

    Update settings_form.dart

---
+++ app_mobile/lib/screens/home/settings_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:app_mobile/shared/constants.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
